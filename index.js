3a32ce82dc8a86169d85ac144bcc39da069a176f
adarsh25t <adarsh25t@gmail.com>
2024-05-20 22:17:07 +0530

    updated

--- index.js
+++ index.js
@@ -10,21 +10,17 @@ import productRouter from './routes/productRouter.js';
 dotenv.config();
 
 const app = express(); 
-// app.use(cors({
-//         origin: process.env.FRONTEND_URL,
-//         credentials: true, 
-// }));
-// app.use(express.json({ limit: '50mb' }));
-// app.use(cookieParser())
+app.use(cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true, 
+}));
+app.use(express.json({ limit: '50mb' }));
+app.use(cookieParser())
 
-// app.use('/api/user',userRoute);
-// app.use('/api/products',productRouter);
+app.use('/api/user',userRoute);
+app.use('/api/products',productRouter);
 
 const PORT = 8080 || process.env.PORT;
-
-app.get('/',(req, res) => {
-    res.send('Server is running')
-})
   
 app.listen(PORT, ()=>{
     connectDB()
