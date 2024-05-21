6a660b3c6198b3bc94910b9d14a372801d38a446
adarsh25t <adarsh25t@gmail.com>
2024-05-21 09:28:49 +0530

    new route change

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ dotenv.config();
 
 const app = express(); 
 app.use(cors({
-        origin: process.env.FRONTEND_URL,
+        origin: ['http://localhost:5173'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true, 
 }));
 app.use(express.json({ limit: '50mb' }));
-app.use(cookieParser())
+app.use(cookieParser());
 
+app.get('/',(req, res) => {
+    res.send('Server is running');
+})
+ 
 // app.use('/api/user',userRoute);
 app.use('/api/products',productRouter);
 
